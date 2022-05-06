c379dd6f661033b6bdb1bc881eebb6a51701a253
Simon <nimon@hotmail.co.uk>
2022-05-06 09:01:23 +0100

    add content

---
+++ module-2/unit-1/workshop-3/server/data/moves.js
@@ -0,0 +1,16 @@
+const moves = [
+    {
+        title: 'Shockwave',
+        owners: ['pikachu']
+    },
+    {
+        title: 'Flamethrower',
+        owners: ['charizard']
+    },
+    {
+        title: 'Body slam',
+        owners: ['pikachu', 'charizard']
+    }
+]
+
+module.exports = moves
